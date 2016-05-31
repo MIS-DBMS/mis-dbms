f24e0899288058f88d8d484fae39dc6870222fc3
moonstarsky37 <moon.starsky37@gmail.com>
2016-05-31 17:43:28 +0800

    SQL commit

--- trust-cafe-1.0/blog_2016-05-03.sql
+++ trust-cafe-1.0/blog_2016-05-03.sql
@@ -1,4 +1,4 @@
-# ************************************************************
+# *************************************************************
 # Sequel Pro SQL dump
 # Version 4529
 #
@@ -6,9 +6,9 @@
 # https://github.com/sequelpro/sequelpro
 #
 # Host: 127.0.0.1 (MySQL 5.5.42)
-# Database: blog
-# Generation Time: 2016-05-03 08:12:45 +0000
-# ************************************************************
+# Database: trust-cafe
+# Generation Time: 2016-05-26 15:49:45 +0000
+# *************************************************************
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -20,89 +20,159 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
-# Dump of table article
+# Dump of table customer
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `article`;
+DROP TABLE IF EXISTS `customer`;
 
-CREATE TABLE `article` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(20) NOT NULL DEFAULT '',
-  `content` text NOT NULL,
-  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `member_id` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `member_id` (`member_id`),
-  CONSTRAINT `article_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table article_tag
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `article_tag`;
-
-CREATE TABLE `article_tag` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `article_id` int(11) unsigned NOT NULL,
-  `tag_id` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `arti` (`article_id`),
-  KEY `ta` (`tag_id`),
-  CONSTRAINT `IDX_01` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  CONSTRAINT `IDX_02` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table comment
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `comment`;
-
-CREATE TABLE `comment` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `member_id` int(11) unsigned NOT NULL,
-  `article_id` int(11) unsigned NOT NULL,
-  `content` text NOT NULL,
-  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `member_id` (`member_id`),
-  KEY `article_id` (`article_id`),
-  CONSTRAINT `IDX02` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  CONSTRAINT `IDX03` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table member
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `member`;
-
-CREATE TABLE `member` (
+CREATE TABLE `customer` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL DEFAULT '',
   `account` varchar(60) NOT NULL DEFAULT '',
   `password` varchar(255) NOT NULL DEFAULT '',
+  `phone` varchar(15),
+  `email` varchar(50) NOT NULL,
+  `jobTitle` varchar(20) NOT NULL DEFAULT '',
+  `address` varchar(50),
+  `birthday` DATE,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
-# Dump of table tag
+# Dump of table customerInterest
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `tag`;
+DROP TABLE IF EXISTS `customerInterest`;
 
-CREATE TABLE `tag` (
+CREATE TABLE `customerInterest` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL DEFAULT '',
+  `interest` ENUM('Monday不魯','Think Big','政大創立方講座','創業肥皂箱','創業計畫書撰寫','東協相關議題','創業','品酒','簡報設計'),//「Monday不魯」「Think Big」「政大創立方講座」「創業肥皂箱」「創業計畫書撰寫」「東協相關議題」
+  `customer_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `customer_id` (customer_id),
+  CONSTRAINT `interest_fk1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table organization
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `organization`;
+
+CREATE TABLE `organization` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `phone` varchar(15),
+  `email` varchar(50) NOT NULL,
+  `address` varchar(50),
+  `miscellaneous` TEXT ,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+
+# Dump of table event
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `event`;
+
+CREATE TABLE `event` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL DEFAULT '',
+  `location` ENUM('B1包場','會議廳','1F包場'),
+  `date` DATE,
+  `startTime` TIME,
+  `endTime` TIME,
+  `description` TEXT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+# Dump of table eventTag
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `eventTag`;
+
+CREATE TABLE `eventTag` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `eventId` int(11) unsigned NOT NULL,
+  `tag` ENUM('Monday不魯','Think Big','政大創立方講座','創業肥皂箱','創業計畫書撰寫','東協相關議題'),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `tag_fk1` FOREIGN KEY (`eventId`) REFERENCES `event` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table item
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `item`;
+
+CREATE TABLE `item` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL DEFAULT '',
+  `date` DATE NOT NULL,
+  `description` TEXT,
+  `value` int(10) unsigned NOT NULL,
+  `customerId` int(11) unsigned NOT NULL,
+  `organizationId` int(11) unsigned NOT NULL,
+  `eventId` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `item_fk1` FOREIGN KEY (`customerId`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `item_fk2` FOREIGN KEY (`organizationId`) REFERENCES `organization` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `item_fk3` FOREIGN KEY (`eventId`) REFERENCES `event` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table hold
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `hold`;
+
+CREATE TABLE `hold` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `type` ENUM('主辦','協辦','贊助'),
+  `organizationId` int(11) unsigned NOT NULL,
+  `eventId` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `hold_fk1` FOREIGN KEY (`organizationId`) REFERENCES `organization` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `hold_fk2` FOREIGN KEY (`eventId`) REFERENCES `event` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+# Dump of table host
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `host`;
+
+CREATE TABLE `host` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `customerId` int(11) unsigned NOT NULL,
+  `eventId` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `host_fk1` FOREIGN KEY (`customerId`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `host_fk2` FOREIGN KEY (`eventId`) REFERENCES `event` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+# Dump of table participate
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `participate`;
+
+CREATE TABLE `participate` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `customerId` int(11) unsigned NOT NULL,
+  `eventId` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `participate_fk1` FOREIGN KEY (`customerId`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `participate_fk2` FOREIGN KEY (`eventId`) REFERENCES `event` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
