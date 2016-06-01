f23d1b2889523c992ee1477a311ef54c948af25f
moonstarsky37 <moon.starsky37@gmail.com>
2016-06-01 15:27:00 +0800

    Commit to routes index.js

--- trust-cafe-1.0/routes/index.js
+++ trust-cafe-1.0/routes/index.js
@@ -1,27 +1,21 @@
 var express = require('express');
 var router = express.Router();
-// var Member = require('../models/Member');
-// var Article = require('../models/Article');
-var Event = require('../models/Event');
+var Customer = require('../models/Customer');
 var Host = require('../models/Host');
 var async = require('async');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Event.getAll(function(err, eventList) {
-  // Article.getAll(function(err, articleList) {
+  Host.getAll(function(err, hostList) {
     if(err) {
       next();
     } else {
       //這邊的做法是使用async each這樣的方式幫我們從articleList中一筆筆去找到member，然後新增一個key叫member在article物件中
-      async.each(eventList, function(event, cb) {
-      // async.each(articleList, function(article, cb) {
-        Host.get(event.eventId, function(err, event) {
-        // Member.get(article.memberId, function(err, member) {
+      async.each(hostList, function(host, cb) {
+        Customer.get(host.customerId, function(err, customer) {
           if(err) {
             cb(err);
           } else {
-            event.type = type;
-            // article.member = member;
+            host.customer = customer;
             cb(null);
           }
         });
@@ -32,10 +26,8 @@ router.get('/', function(req, res, next) {
         } else {
           res.render('index',
           {
-            type : req.session.type || null,
-            eventList: eventList
-            // member : req.session.member || null,
-            // articleList: articleList
+            customer : req.session.customer || null,
+            customerList: customerList
           });
         }
       });
