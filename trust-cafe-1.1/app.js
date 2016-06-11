051b71143492ad7d7c11071bf102d0e8175ea8ce
BrLi <rainman59118@gmail.com>
2016-06-11 22:55:30 +0800

    apps.js, another attempt

--- trust-cafe-1.1/app.js
+++ trust-cafe-1.1/app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var passport = require('passport');
 var flash = require('req-flash');
-
 var routes = require('./routes/index');
 var signup = require('./routes/signup');
 var users = require('./routes/users');
@@ -25,16 +25,15 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(session({secret : 'HelloExpressSESSION'}));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser('123456789'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser(''));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(flash());
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 // use (用在url的哪裡, 哪個已宣告的routes變數)
 app.use('/', routes);
@@ -50,6 +49,15 @@ app.use('/customer', customer);
 app.use('/updateEvent', updateEvent);
 // app.use('/event', register);
 
+// required for passport
+app.use(session({
+	secret: 'vidyapathaisalwaysrunning',
+	resave: true,
+	saveUninitialized: false,
+  cookie: { secure: true }
+ } )); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
