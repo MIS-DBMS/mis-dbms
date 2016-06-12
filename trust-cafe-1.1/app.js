4d03cc05b9b44d22c41b25737feb14e195198df1
BrLi <rainman59118@gmail.com>
2016-06-13 02:03:19 +0800

    try again

--- trust-cafe-1.1/app.js
+++ trust-cafe-1.1/app.js
@@ -5,9 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-
+var passport = require('passport');
 var flash = require('req-flash');
-
 var routes = require('./routes/index');
 var signup = require('./routes/signup');
 var users = require('./routes/users');
@@ -27,8 +26,13 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(session({secret : 'HelloExpressSESSION'}));
-
+app.use(session({
+  secret : 'HelloExpressSESSION',
+  resave: true,
+  saveUninitialized: true
+  }));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
